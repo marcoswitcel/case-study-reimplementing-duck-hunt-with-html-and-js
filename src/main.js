1db68d530e9017047680b0e4eb1cee10f1fd89c5
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-10-27 13:29:05 -0300

    configurando tela pra testes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import { createCanvas } from './utils.js';
+
 console.log('olá mundo duck hunt');
+
+const canvas = createCanvas(256, 240, document.body);
+canvas.style.imageRendering = 'pixelated';
+const ctx = canvas.getContext('2d');
+
+// background
+ctx.fillStyle = '#4da4ff';
+ctx.fillRect(0, 0, 256, 240);
