64fb8397f450009f025d6c8115dc91429cbccd47
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-10-27 18:02:33 -0300

    desenhando um sprite pra ver o resultado

--- src/main.js
+++ src/main.js
@@ -9,3 +9,22 @@ const ctx = canvas.getContext('2d');
 // background
 ctx.fillStyle = '#4da4ff';
 ctx.fillRect(0, 0, 256, 240);
+
+
+const image = new Image;
+
+image.src = '/public/assets/NES - Duck Hunt - The Dog.png';
+
+function main(timestamp) {
+    if (!timestamp) requestAnimationFrame(main);
+    
+    let i = ~~((timestamp / (1000 / 6)) % 4);
+    console.log(i)
+    ctx.drawImage(image, i * 56, 13, 56, 44, 0, 0, 56, 44);
+
+    requestAnimationFrame(main);
+}
+
+image.addEventListener('load', () => {
+    main();
+});
