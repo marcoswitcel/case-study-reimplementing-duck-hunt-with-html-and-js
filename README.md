f75ae82af91be0e0d6e6ed2bb9b5a42ad0facb9a
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-10-28 23:01:20 -0300

    primeira versão para testes da classe sprite animado

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Organizando...
 https://www.spriters-resource.com/nes/duckhunt/
 https://hackaday.com/2015/11/16/resurrecting-duckhunt/
 https://en.wikipedia.org/wiki/Duck_Hunt
+
+# Ferramentas e tutoriais
+
+* gimp: [https://thegimptutorials.com/how-to-remove-color/](https://thegimptutorials.com/how-to-remove-color/)
--- src/main.js
+++ src/main.js
@@ -9,7 +9,57 @@ canvas.style.imageRendering = 'pixelated';
 const ctx = canvas.getContext('2d');
 
 
+class RenderInfo {
+    /**
+     * @type {CanvasImageSource}
+     */
+    image;
 
+    source_x; source_y;
+    source_W; source_H;
+
+    targe_x; targe_y;
+    targe_W; targe_H;
+
+    constructor(...args) {
+        console.assert([3, 5, 9].indexOf(args.length) !== -1);
+        this.image = args[0];
+        this.source_x = args[1];
+        this.source_y = args[2];
+        this.source_W = args[3];
+        this.source_H = args[4];
+        this.targe_x = args[5];
+        this.targe_y = args[6];
+        this.targe_W = args[7];
+        this.targe_H = args[8];
+    }
+
+    get args() {
+        return [
+            this.image,
+            this.source_x, this.source_y,
+            this.source_W, this.source_H,
+            this.targe_x, this.targe_y,
+            this.targe_W, this.targe_H,
+        ]
+    }
+}
+
+class AnimatedSprite {
+
+    constructor(image, sequenceOfFramesInfo, time) {
+        this.image = image;
+        this.sequenceOfFramesInfo = sequenceOfFramesInfo;
+        this.time = time;
+    }
+
+    renderInfo() {
+        // @todo João, faze funcionar de verdade, usar o tempo
+        const frame = this.sequenceOfFramesInfo[0];
+
+        return new RenderInfo(this.image, ...frame)
+    }
+}
 
 
 const image = new Image;
@@ -31,6 +81,14 @@ function main(timestamp) {
     let offsetY = NES.height * 0.6;
     console.log(i)
     ctx.drawImage(image, i * 56, 13, 56, 44, offsetX, offsetY, 56, 44);
+
+    // @todo João, remover assim que terminar de testar
+    teste: {
+        const animatedSprite = new AnimatedSprite(image, [[
+            0 * 56, 13, 56, 44, offsetX, offsetY - 100, 56, 44
+        ]], 0);
+        ctx.drawImage(...(animatedSprite.renderInfo().args));
+    }
 
     requestAnimationFrame(main);
 }
